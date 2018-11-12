291207db5f9d34f3738c0a6266998e2cc753a2a8
Paterakis <P18825@u-ssi.net>
2018-11-11 20:34:37 -0600

    final results

---
+++ PyBank/main.py
@@ -0,0 +1,34 @@
+#Your task is to create a Python script that analyzes the records to calculate each of the following:
+
+import os
+import csv
+row_count = 0
+
+csvpath = os.path.join('.\Resources', 'budget_data.csv')
+
+with open(csvpath, newline='') as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    # Read the header row first (skip this step if there is no header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header
+    for row in csvreader:
+        print(row)
+        row_count = row_count+1
+
+#   The total number of months included in the dataset
+print(f"Total months: {row_count}")
+
+#    The total net amount of "Profit/Losses" over the entire period
+
+#   The average change in "Profit/Losses" between months over the entire period
+
+#   The greatest increase in profits (date and amount) over the entire period
+
+#   The greatest decrease in losses (date and amount) over the entire period
